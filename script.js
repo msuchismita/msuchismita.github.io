965209b4302932ac80f3314dfa2097929721fa0f
Suchismita Moharana <suchismita@Suchis-MBP.attlocal.net>
2019-11-29 13:29:38 -0600

    adding separate style sheets and js files

---
+++ script.js
@@ -0,0 +1,3 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Hello Dear Visitor!</br> We are happy that you've chosen our website to learn programming languages. We're sure you'll become one of the best programmers in your country. Good luck to you!";
+}

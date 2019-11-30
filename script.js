b50d0bfeb673f014fc0e9e61eb765d2e7e27f2dc
Suchismita Moharana <suchismita@Suchis-MBP.attlocal.net>
2019-11-29 19:45:53 -0600

    Changing the page style

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 function myFunction() {
     document.getElementById("demo").innerHTML = "Hello Dear Visitor!</br> We are happy that you've chosen our website to learn programming languages. We're sure you'll become one of the best programmers in your country. Good luck to you!";
 }
+
+
+
